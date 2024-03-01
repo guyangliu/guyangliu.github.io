422cef301ac595ac748c971d459cc9d94551c755
guyangliu <2604462905@qq.com>
2024-03-01 10:17:37 +0800

    comment latex

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       devShells."${system}".default = pkgs.mkShell {
         buildInputs = with pkgs; [
           inotify-tools
-          (texlive.combine { inherit (texlive) scheme-medium standalone tikz-cd; })
+          # (texlive.combine { inherit (texlive) scheme-medium standalone tikz-cd; })
           forester.packages."${system}".default
         ];
         shellHook = ''
